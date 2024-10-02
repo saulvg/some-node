dfc2bbaf1e8a21aa70afe56cd5444b58039cc253
saulvg <saulvgproyecto@gmail.com>
2024-10-02 20:46:42 +0200

    create simple http server

--- lesson-2/1.http.js
+++ lesson-2/1.http.js
@@ -1,11 +1,44 @@
 const http = require("node:http"); //Protocolo http -> protocolo de transferencia de hipertextos
+const fs = require("node:fs");
+const path = require("node:path");
 
 const desirePort = process.env.PORT ?? 1234;
 
-const server = http.createServer((req, res) => {
-  console.log("request recived: ", req.url);
-  res.end("Hola mundo");
-});
+const processRequest = (req, res) => {
+  /* console.log("request recived: ", req.url);
+  res.end("Hola mundo"); */
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    //res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1>Bienvenido a mi página de inicio</h1>");
+  } else if (req.url === "/imagen-super-bonita.png") {
+    const imagePath = path.join(__dirname, "imagenDemo.png");
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        console.error("Error al leer el archivo:", err); // Log del error para depurar
+        res.statusCode = 500;
+        res.end("<h1>500 Internal Server Error</h1>");
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "image/png");
+        res.end(data);
+      }
+    });
+  } else if (req.url === "/contacto") {
+    res.statusCode = 200;
+    //res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1>Bienvenido a mi página de Contancto</h1>");
+  } else {
+    res.statusCode = 404;
+    //res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1>404</h1>");
+  }
+};
+
+const server = http.createServer(processRequest);
 
 server.listen(desirePort, () => {
   console.log(`Server lisening on port http://localhost:${desirePort}`);
